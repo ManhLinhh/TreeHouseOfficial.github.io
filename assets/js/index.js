117ab3aed403fc649f337679285048ed7d675927
Manh Linh <62207907+TreeHouseOfficial@users.noreply.github.com>
2020-09-11 12:08:36 +0700

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+function openRegisterForm() {
+    document.getElementById("modal__register").style.display = "flex";
+}
+
+function openLoginForm() {
+    let x = document.getElementById("modal__login").style.display = "flex";
+}
+
+function closeLoginForm() {
+    let x = document.getElementById("modal__login").style.display = "none";
+}
+
+function closeRegisterForm() {
+    let x = document.getElementById("modal__register").style.display = "none";
+}
